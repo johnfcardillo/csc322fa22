e4646764ebee6a3857eafe3a09758003b7208766
johnfcardillo <124123667+johnfcardillo@users.noreply.github.com>
2023-09-14 23:30:42 -0400

    2 files modified

--- hw1.2.js
+++ hw1.2.js
@@ -1,27 +1,23 @@
 import * as THREE from 'three';
 
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const scene = new THREE.Scene();
+camera.position.z = 5;
 
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0xff00dc } );
-
-const points = [];
-points.push( new THREE.Vector3( - 20, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 20, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-points.push( new THREE.Vector3( - 20, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-scene.add( line );
-renderer.render( scene, camera );
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
