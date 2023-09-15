f62d60bf76f62da4b4f3e7d21bff81996889263a
johnfcardillo <124123667+johnfcardillo@users.noreply.github.com>
2023-09-14 23:31:15 -0400

    1 file modified

--- hw1.2.js
+++ hw1.2.js
@@ -19,5 +19,7 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+  cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
 }
 animate();
